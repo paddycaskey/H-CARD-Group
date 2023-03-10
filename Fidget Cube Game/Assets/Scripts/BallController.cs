589809c58e78db1d83f398431cd12f783889226e
NabeelAzuhar <nabeel_azuhar@yahoo.com.sg>
2023-03-10 12:33:40 +0000

    unity project creation complete along with gitignore

--- Fidget Cube Game/Assets/Scripts/BallController.cs
+++ Fidget Cube Game/Assets/Scripts/BallController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class BallController : MonoBehaviour {
 
     private Rigidbody2D RB;
     private Vector2 currentPos;
