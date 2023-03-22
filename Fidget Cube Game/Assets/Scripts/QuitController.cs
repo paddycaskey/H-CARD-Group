273419b59e70afb87ec9aaff7591544879ed276f
NabeelAzuhar <nabeel_azuhar@yahoo.com.sg>
2023-03-22 18:18:00 +0000

    fixed minor bugs and exported game

--- Fidget Cube Game/Assets/Scripts/QuitController.cs
+++ Fidget Cube Game/Assets/Scripts/QuitController.cs
@@ -6,11 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class QuitController : MonoBehaviour
 {
-     
+    public GameObject eventSystem;
+    public GameObject yesBtn;
+    
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Image>().sprite = GameManager.instance.avatar;    
+    }
+
+    void Update()
+    {
+        // if eventSystem is not selecting anything, select the yes button
+        if (eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().currentSelectedGameObject == null)
+        {
+            eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(yesBtn);
+        }
     }
 
     public void ReturnToMenu()
