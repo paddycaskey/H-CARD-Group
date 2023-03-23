b0090a1de75a004b4b5a6c51b2ae901c0a7ca3dd
paddycaskey <115624335+paddycaskey@users.noreply.github.com>
2023-03-23 22:56:21 +0000

    Add files via upload

---
+++ Controller_Code_Ver3.ino
@@ -0,0 +1,153 @@
+#include <HID.h>
+#include <Keyboard.h>
+
+const int button1Pin = 2;
+const int button2Pin = 3;
+
+const int inputDTPin = 6;
+const int inputCLKPin = 7;
+
+const int switchPin = 9;
+
+const int pressurePin = A0;
+
+const int xaxisPin = A2;
+const int yaxisPin = A3;
+
+const int motorPin = 12;
+
+int counter = 0;
+int turnLimit = 10;
+
+int currentStateCLK;
+int previousStateCLK;
+
+int previousSwitch = 0;
+
+int button1State = 0;
+int button2State = 0;
+
+int switchState = 0;
+
+int x;
+int y;
+
+int joystickThresholdup  = 600;
+int joystickThresholddown = 320;
+
+int force;
+int forceThreshold = 500;
+
+void setup() {
+  pinMode(button1Pin, INPUT);
+  pinMode(button2Pin, INPUT);
+
+  pinMode(inputDTPin, INPUT);
+  pinMode(inputCLKPin, INPUT);
+
+  pinMode(switchPin, INPUT);
+
+  pinMode(pressurePin, INPUT);
+
+  pinMode(xaxisPin, INPUT);
+  pinMode(yaxisPin, INPUT);
+
+  pinMode(motorPin, OUTPUT);
+
+  Serial.begin(9600);
+  Keyboard.begin();
+
+  previousStateCLK = digitalRead(inputCLKPin);
+}
+
+void loop() {
+  button1State = digitalRead(button1Pin);
+  button2State = digitalRead(button2Pin);
+
+  currentStateCLK = digitalRead(inputCLKPin);
+  if (currentStateCLK != previousStateCLK){
+
+    if (digitalRead(inputDTPin) != currentStateCLK){
+      counter ++;
+    } else{
+      counter --;
+    }
+
+    previousStateCLK = currentStateCLK;
+  }
+  
+  switchState = digitalRead(switchPin);
+  
+  x = analogRead(xaxisPin);
+  y = analogRead(yaxisPin);
+  
+  force = analogRead(pressurePin);
+  
+  if (button1State == HIGH && button2State == HIGH) {
+      Serial.println("Buttons 1 & 2 Pressed");
+      Keyboard.press(' ');
+      digitalWrite(motorPin, HIGH);
+    } else {
+      Keyboard.release(' ');
+    }
+  
+  if (counter >= turnLimit || counter <= -turnLimit) {
+      Serial.println("Encoder Turned");
+      Keyboard.press('b');
+      digitalWrite(motorPin, HIGH);
+      counter = 0;
+    } else {
+      Keyboard.release('b');
+    }
+
+  if (x >= joystickThresholdup) {
+      Serial.println("Left Pressed");
+      Keyboard.press(KEY_LEFT_ARROW);
+      digitalWrite(motorPin, HIGH);
+    } else {
+      Keyboard.release(KEY_LEFT_ARROW);
+    }
+  
+  if (x <= joystickThresholddown) {
+      Serial.println("Right Pressed");
+      Keyboard.press(KEY_RIGHT_ARROW);
+      digitalWrite(motorPin, HIGH);
+    } else {
+      Keyboard.release(KEY_RIGHT_ARROW);
+    }
+
+      if (y >= joystickThresholdup) {
+      Serial.println("Up Pressed");
+      Keyboard.press(KEY_UP_ARROW);
+      digitalWrite(motorPin, HIGH);
+    } else {
+      Keyboard.release(KEY_UP_ARROW);
+    }
+  
+  if (y <= joystickThresholddown) {
+      Serial.println("Down Pressed");
+      Keyboard.press(KEY_DOWN_ARROW);
+      digitalWrite(motorPin, HIGH);
+    } else {
+      Keyboard.release(KEY_DOWN_ARROW);
+    }
+
+   if (force >= forceThreshold) {
+      Serial.println("Pressed with force");
+      Keyboard.press('n');
+      digitalWrite(motorPin, HIGH);
+    } else {
+      Keyboard.release('n');
+    }
+
+  if (switchState != previousSwitch) {
+      Serial.println("Switch On");
+      Keyboard.press('m');
+      digitalWrite(motorPin, HIGH);
+    } else {
+      Keyboard.release('m');
+    }
+
+  previousSwitch = switchState;
+  digitalWrite(motorPin, LOW);
+}
